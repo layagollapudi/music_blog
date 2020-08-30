48d93118e01694f45767b19808ce24efc5cd3f07
Laya Gollapudi <layagollapudi@layas-mbp.lan>
2020-08-30 11:50:34 -0700

    ordering

--- api/blog.py
+++ api/blog.py
@@ -38,14 +38,22 @@ Query parameters can be provided for more filtration, which include:
 > date = date on which the post was written
 > song_title = name of the song which the post is about
 > song_artist = name of the artist who the post is about
+> reverse = if to send order from least->greatest instead (defaults to false)
 """
 @bp.route("/ordered/<string:order_key>", methods=["GET"])
 def get_all_blog_posts(order_key):
     # Get and populate argument dictionary for mongo query
     query_params = ['title', 'author', 'date', 'song_title', 'song_artist']
+
+    # We don't want to order on a key that we are filtering on, so we remove it from the list
+    if order_key in query_params:
+        query_params.remove(order_key)
+
     args = make_args_dict(request, query_params)
 
-    # We don't want to order on a key that we are filtering on, so we remove it from the dict
-    if order_key in args: del args[order_key]
+    # We want to add the needed prefix for ordering directionality
+    if 'reverse' in request.args and bool(request.args.get('reverse')) == True:
+        order_key = '-' + order_key
 
+    posts = BlogPost.objects(**args).order_by(order_key).to_json()
     return Response({}, mimetype="application/json", status=200)
