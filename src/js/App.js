00dd489c9800db608f83eeeba025d419c8f4ef18
Laya Gollapudi <layagollapudi@layas-mbp.lan>
2020-10-11 21:18:51 -0700

    refactoring

--- src/js/App.js
+++ src/js/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Home from "./components/Home";
 import Post from "./components/Post";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: { main: '#30D5C8' },
-    secondary: { main: '#FFC2C7' },
-    whiteText: { main: '#FFFFFF' },
-    darkText: { main: '#0e4642' },
-    midText: { main: '#1b877f' },
-  },
-});
+import { theme } from "./helpers/paletteTheme";
 
 export default function App() {
   return (
---
+++ src/js/helpers/paletteTheme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#30D5C8' },
+    secondary: { main: '#FFC2C7' },
+    whiteText: { main: '#FFFFFF' },
+    darkText: { main: '#0e4642' },
+    midText: { main: '#1b877f' },
+  },
+});
